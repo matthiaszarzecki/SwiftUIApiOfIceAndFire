8dbf48d4f9f7ebfe0940f5d5d68ab5d80974e82b
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-01-26 14:22:05 +0100

    Add comments to StringExtensionTests

--- GotTest/GotTestTests/StringExtensionTests.swift
+++ GotTest/GotTestTests/StringExtensionTests.swift
@@ -11,12 +11,24 @@ import XCTest
 class StringExtensionTests: XCTestCase {
   
   func testIsLink() throws {
+    // GIVEN a string that is a hyperlink
     let character = MockClasses.characterLink
-    XCTAssertTrue(character.isLink)
+    
+    // WHEN we check if that is indeed a link to access further data
+    let result = character.isLink
+    
+    // THEN true is returned
+    XCTAssertTrue(result)
   }
   
   func testIsNotLink() throws {
+    // GIVEN a string that is not a hyperlink
     let house = MockClasses.houseName
-    XCTAssertFalse(house.isLink)
+    
+    // WHEN we check if that is indeed a link to access further data
+    let result = house.isLink
+    
+    // THEN false is returned
+    XCTAssertFalse(result)
   }
 }
