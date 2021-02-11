4c02bfcbfe8a38c3ebfd7982ff6eeb2be2a4cb1b
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-02-11 01:13:12 +0100

    Create colorParser shell

---
+++ HousesOfASOIAF/HousesOfASOIAF/Helpers/ColorParser.swift
@@ -0,0 +1,16 @@
+//
+//  ColorParser.swift
+//  HousesOfASOIAF
+//
+//  Created by Matthias Zarzecki on 11.02.21.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorParser {
+  func getColors(fromString text: String) -> [Color] {
+    var colors: [Color] = []
+    return colors
+  }
+}
