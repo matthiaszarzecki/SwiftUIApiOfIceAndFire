5084c5da642f0ba80593c7cfb634ca8e1dcd557f
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-02-16 23:41:47 +0100

    Add hasAtLeastOneItem to string-array-extension

--- HousesOfASOIAF/HousesOfASOIAF/Extensions/String-Array-Extension.swift
+++ HousesOfASOIAF/HousesOfASOIAF/Extensions/String-Array-Extension.swift
@@ -8,8 +8,13 @@
 import Foundation
 
 extension Array where Element == String {
-  /// Returns true if the array has usable entries
-  /// (>0 elements and first elements not an empty string).
+  /// Returns true if the array contains at least
+  /// one entry that is also in the passed array.
+  func hasAtLeastOneItem(fromArray array: [String]) -> Bool {
+    return self.contains(where: array.contains)
+  }
+  
+  /// Returns true if the array has usable entries.
   var hasNonEmptyEntries: Bool {
     return self.hasEntries && self[0] != ""
   }
--- HousesOfASOIAF/HousesOfASOIAF/Helpers/ColorParser.swift
+++ HousesOfASOIAF/HousesOfASOIAF/Helpers/ColorParser.swift
@@ -31,89 +31,89 @@ struct ColorParser {
     var words = adaptedText.components(separatedBy: " ")
     
     // Remove empty array entries
-    words.removeAll { $0 == "" }
+    words.removeAll { $0 == "" || $0 == " " }
     
     // Check if any words that describe colors are present, and
     // if so add the corresponding color to the return-collection.
     let synonymsForYellow = ["yellow", "gold", "golden", "or", "sun"]
-    if words.contains(where: synonymsForYellow.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForYellow) {
       colors.append(.yellow)
     }
     
     let synonymsForOrange = ["orange", "tenné", "tenny", "ochre", "copper", "saffron", "rusty", "black-and-orange", "bronze"]
-    if words.contains(where: synonymsForOrange.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForOrange) {
       colors.append(.orange)
     }
     
-    let synonymsForBrown = ["brown", "ermine", "erminois", "oaken", "oak", "tree"]
-    if words.contains(where: synonymsForBrown.contains) {
+    let synonymsForBrown = ["brown", "ermine", "erminois", "oaken", "oak", "tree", "moose"]
+    if words.hasAtLeastOneItem(fromArray: synonymsForBrown) {
       let customBrownColor = Color(hex: "773D1C")
       colors.append(customBrownColor)
     }
     
     let synonymsForRed = ["red", "gules", "sanguine", "fiery", "blood", "bloody", "pommes"]
-    if words.contains(where: synonymsForRed.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForRed) {
       colors.append(.red)
     }
     
     let synonymsForPink = ["pink", "carnation"]
-    if words.contains(where: synonymsForPink.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForPink) {
       let customPinkColor = Color(hex: "E87AA4")
       colors.append(customPinkColor)
     }
     
     let synonymsForPurple = ["purple", "purpure", "murrey", "grape", "amaranth"]
-    if words.contains(where: synonymsForPurple.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForPurple) {
       colors.append(.purple)
     }
     
     let synonymsForBlue = ["blue", "azure", "indigo", "fountain"]
-    if words.contains(where: synonymsForBlue.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForBlue) {
       colors.append(.blue)
     }
     
     let synonymsForLightBlue = ["sky", "celeste", "sky-blue"]
-    if words.contains(where: synonymsForLightBlue.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForLightBlue) {
       let customLightBlueColor = Color(hex: "87CEEB")
       colors.append(customLightBlueColor)
     }
     
     let synonymsForCyan = ["cyan", "turquoise"]
-    if words.contains(where: synonymsForCyan.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForCyan) {
       let customCyanColor = Color(hex: "00FFFF")
       colors.append(customCyanColor)
     }
     
     let synonymsForLightGreen = ["pale-green"]
-    if words.contains(where: synonymsForLightGreen.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForLightGreen) {
       let customLightGreenColor = Color(hex: "77C344")
       colors.append(customLightGreenColor)
     }
     
     let synonymsForGreen = ["green", "vert"]
-    if words.contains(where: synonymsForGreen.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForGreen) {
       colors.append(.green)
     }
     
     let synonymsForDarkGreen = ["dark-green"]
-    if words.contains(where: synonymsForDarkGreen.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForDarkGreen) {
       let customDarkGreenColor = Color(hex: "134B0E")
       colors.append(customDarkGreenColor)
     }
 
     let synonymsForWhite = ["white", "argent", "silver", "ice", "fountain"]
-    if words.contains(where: synonymsForWhite.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForWhite) {
       let customWhiteColor = Color(hex: "F2F2F7")
       colors.append(customWhiteColor)
     }
      
     let synonymsForGray = ["grey", "gray", "gules", "cendrée"]
-    if words.contains(where: synonymsForGray.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForGray) {
       colors.append(.gray)
     }
     
     let synonymsForBlack = ["black", "sable", "black-and-orange"]
-    if words.contains(where: synonymsForBlack.contains) {
+    if words.hasAtLeastOneItem(fromArray: synonymsForBlack) {
       let customBlackColor = Color(hex: "111111")
       colors.append(customBlackColor)
     }
