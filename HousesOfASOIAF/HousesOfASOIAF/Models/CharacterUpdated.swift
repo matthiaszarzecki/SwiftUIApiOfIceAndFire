113b46985347f2fc0c984a3453a94865652238a3
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-03-02 01:01:17 +0100

    Add init to CharacterUpdated

--- HousesOfASOIAF/HousesOfASOIAF/Models/CharacterUpdated.swift
+++ HousesOfASOIAF/HousesOfASOIAF/Models/CharacterUpdated.swift
@@ -11,5 +11,27 @@ import SwiftUI
 /// The character-data but with the URL's called
 /// and replaced by their corresponding data.
 struct CharacterUpdated {
+  let id: String
+  let name: String
+  let culture: String
+  let born: String
+  let died: String
+  let titles: [String]
+  let aliases: [String]
+  let tvShowAppearances: [String]
+  let portrayedBy: [String]
   
+  init(
+    fromCharacter character: CharacterBasic
+  ) {
+    id = character.id
+    name = character.name
+    culture = character.culture
+    born = character.born
+    died = character.died
+    titles = character.titles
+    aliases = character.aliases
+    tvShowAppearances = character.tvShowAppearances
+    portrayedBy = character.portrayedBy
+  }
 }
