43d61e55c62b707d93cce0783dc8fa91c3bb8f11
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-04-15 10:55:15 +0200

    Add identifiable protocol to character model

--- HousesOfWesteros/HousesOfWesteros/Models/CharacterBasic.swift
+++ HousesOfWesteros/HousesOfWesteros/Models/CharacterBasic.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Contains all data related to a Character in ASOIAF.
-struct CharacterBasic: Codable, Hashable {
+struct CharacterBasic: Codable, Identifiable, Hashable {
   // The Api ALWAYS returns a string for each field. If a field
   // is not assigned it will return an empty string "".
   
