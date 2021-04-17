b455d15ade1f10ba55f5f64cfe3ae6ea90023442
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-04-17 14:22:33 +0200

    Separate String Unit Tests

---
+++ HousesOfWesteros/HousesOfWesterosTests/StringExtensionTests/StringFirstTests.swift
@@ -0,0 +1,33 @@
+//
+//  StringExtensionTests.swift
+//  GotTestTests
+//
+//  Created by Matthias Zarzecki on 25.01.21.
+//
+
+import XCTest
+@testable import HousesOfWesteros
+
+class StringFirstTests: XCTestCase {
+  func testFirst() {
+    // GIVEN a string
+    let character = "Delonne Allyrion"
+    
+    // WHEN we call the "first" computed variable
+    let result = character.first
+    
+    // THEN the first character is returned
+    XCTAssertEqual(result, "D")
+  }
+  
+  func testFirstOnEmptyString() {
+    // GIVEN an empty string
+    let character = ""
+    
+    // WHEN we call the "first" computed variable
+    let result = character.first
+    
+    // THEN an empty string is returned
+    XCTAssertEqual(result, "")
+  }
+}
--- HousesOfWesteros/HousesOfWesterosTests/StringExtensionTests/StringSubscriptingTests.swift
+++ HousesOfWesteros/HousesOfWesterosTests/StringExtensionTests/StringSubscriptingTests.swift
@@ -1,43 +1,14 @@
 //
-//  StringExtensionTests.swift
-//  GotTestTests
+//  StringSubscriptingTests.swift
+//  HousesOfWesterosTests
 //
-//  Created by Matthias Zarzecki on 25.01.21.
+//  Created by Matthias Zarzecki on 17.04.21.
 //
 
 import XCTest
 @testable import HousesOfWesteros
 
-class StringExtensionTests: XCTestCase {
-  
-  //MARK: - isLink Tests
-
-  func testIsLink() {
-    // GIVEN a string that is a hyperlink
-    let string = "https://www.anapioficeandfire.com/api/characters/290"
-    
-    // WHEN we check if that is indeed a
-    // link to access further data
-    let result = string.isLink
-    
-    // THEN true is returned
-    XCTAssertTrue(result)
-  }
-  
-  func testIsNotLink() {
-    // GIVEN a string that is not a hyperlink
-    let string = "House Allyrion of Godsgrace"
-    
-    // WHEN we check if that is indeed a
-    // link to access further data
-    let result = string.isLink
-    
-    // THEN false is returned
-    XCTAssertFalse(result)
-  }
-  
-  //MARK: - Subscripting Tests
-  
+class StringSubscriptingTests: XCTestCase {
   func testSubScriptingAStringWithIndexZero() {
     // GIVEN a string
     let character = "Delonne Allyrion"
@@ -115,30 +86,6 @@ class StringExtensionTests: XCTestCase {
     
     // WHEN we subscript it with a negative index
     let result = character[-1]
-    
-    // THEN an empty string is returned
-    XCTAssertEqual(result, "")
-  }
-  
-  // MARK: - First Tests
-  
-  func testFirst() {
-    // GIVEN a string
-    let character = "Delonne Allyrion"
-    
-    // WHEN we call the "first" computed variable
-    let result = character.first
-    
-    // THEN the first character is returned
-    XCTAssertEqual(result, "D")
-  }
-  
-  func testFirstOnEmptyString() {
-    // GIVEN an empty string
-    let character = ""
-    
-    // WHEN we call the "first" computed variable
-    let result = character.first
     
     // THEN an empty string is returned
     XCTAssertEqual(result, "")
