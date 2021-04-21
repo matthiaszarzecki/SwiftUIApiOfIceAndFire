b48225b2d385e553b90b6436cec57e609b7eaaba
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-04-21 21:23:50 +0200

    Improve comment for greatHouses, move Constants

---
+++ HousesOfWesteros/HousesOfWesteros/Helpers/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  HousesOfWesteros
+//
+//  Created by Matthias Zarzecki on 18.04.21.
+//
+
+import Foundation
+
+struct Constants {
+  /// The Ids of the 9 great houses:
+  /// - Targaryen of King's Landing
+  /// - Stark of Winterfell
+  /// - Lannister of Casterly Rock
+  /// - Arryn of the Eyrie
+  /// - Tully of Riverrun
+  /// - Greyjoy of Pyke
+  /// - Baratheon of Storm's End
+  /// - Tyrell of Highgarden
+  /// - Martell of Sunspear
+  static let greatHouses = [7, 17, 169, 229, 285, 362, 378, 395, 398]
+}
