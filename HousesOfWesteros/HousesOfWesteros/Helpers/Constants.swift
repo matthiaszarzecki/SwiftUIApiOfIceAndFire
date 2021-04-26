4e0c8526331f92f7228f69c184405ae29b270764
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-04-26 20:47:16 +0200

    Update readme

--- HousesOfWesteros/HousesOfWesteros/Helpers/Constants.swift
+++ HousesOfWesteros/HousesOfWesteros/Helpers/Constants.swift
@@ -10,15 +10,15 @@ import SwiftUI
 
 struct Constants {
   /// The Ids of the 9 great houses:
-  /// 378 - Targaryen of King's Landing
-  /// 362 - Stark of Winterfell
-  /// 229 - Lannister of Casterly Rock
   ///   7 - Arryn of the Eyrie
-  /// 395 - Tully of Riverrun
-  /// 169 - Greyjoy of Pyke
   ///  17 - Baratheon of Storm's End
-  /// 398 - Tyrell of Highgarden
+  /// 169 - Greyjoy of Pyke
+  /// 229 - Lannister of Casterly Rock
   /// 285 - Martell of Sunspear
+  /// 362 - Stark of Winterfell
+  /// 378 - Targaryen of King's Landing
+  /// 395 - Tully of Riverrun
+  /// 398 - Tyrell of Highgarden
   static let greatHouses = [7, 17, 169, 229, 285, 362, 378, 395, 398]
   
   /// Returns a lighter version of the
