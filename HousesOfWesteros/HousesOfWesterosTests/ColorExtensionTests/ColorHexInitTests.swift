366ae462e747866d394fe1cb8725b7ff54964b2e
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-05-19 13:10:52 +0200

    Add more color-tests

--- HousesOfWesteros/HousesOfWesterosTests/ColorExtensionTests/ColorHexInitTests.swift
+++ HousesOfWesteros/HousesOfWesterosTests/ColorExtensionTests/ColorHexInitTests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import HousesOfWesteros
 
 class ColorHexInitTests: XCTestCase {
-  func testHexInitRedWithHashSign() {
+  func testHexInitColorWithHashSign() {
     // GIVEN a hex-string that encodes "red" beginning with a hashsign
     let hex = "#FF0000"
     
@@ -23,7 +23,7 @@ class ColorHexInitTests: XCTestCase {
     XCTAssertEqual(result, colorRed)
   }
   
-  func testHexInitRedWithoutHashSign() {
+  func testHexInitColorWithoutHashSign() {
     // GIVEN a hex-string that encodes "red" without a hashsign
     let hex = "FF0000"
     
@@ -34,6 +34,58 @@ class ColorHexInitTests: XCTestCase {
     // to a regularly initialized SwiftUI Color.
     let colorRed = Color(.sRGB, red: 1.0, green: 0.0, blue: 0.0, opacity: 1.0)
     XCTAssertEqual(result, colorRed)
+  }
+  
+  func testHexInitYellow() {
+    // GIVEN a hex-string that encodes "yellow"
+    let hex = "FFFF00"
+    
+    // WHEN we create a color from that string
+    let result = Color(hex: hex)
+
+    // THEN the resulting SwiftUI Color is identical
+    // to a regularly initialized SwiftUI Color.
+    let colorYellow = Color(.sRGB, red: 1.0, green: 1.0, blue: 0.0, opacity: 1.0)
+    XCTAssertEqual(result, colorYellow)
+  }
+  
+  func testHexInitPurple() {
+    // GIVEN a hex-string that encodes "purple"
+    let hex = "FF00FF"
+    
+    // WHEN we create a color from that string
+    let result = Color(hex: hex)
+
+    // THEN the resulting SwiftUI Color is identical
+    // to a regularly initialized SwiftUI Color.
+    let colorPurple = Color(.sRGB, red: 1.0, green: 0.0, blue: 1.0, opacity: 1.0)
+    XCTAssertEqual(result, colorPurple)
+  }
+  
+  func testHexInitCyan() {
+    // GIVEN a hex-string that encodes "cyan"
+    let hex = "00FFFF"
+    
+    // WHEN we create a color from that string
+    let result = Color(hex: hex)
+
+    // THEN the resulting SwiftUI Color is identical
+    // to a regularly initialized SwiftUI Color.
+    let colorCyan = Color(.sRGB, red: 0.0, green: 1.0, blue: 1.0, opacity: 1.0)
+    XCTAssertEqual(result, colorCyan)
+  }
+  
+  func testHexInitBlack() {
+    // GIVEN a hex-string that encodes "cyan"
+    let hex = "000000"
+    
+    // WHEN we create a color from that string
+    let result = Color(hex: hex)
+
+    // THEN the resulting SwiftUI Color is identical
+    // to a regularly initialized SwiftUI Color.
+    let colorBlack = Color(.sRGB, red: 0.0, green: 0.0, blue: 0.0, opacity: 1.0)
+    XCTAssertEqual(result, colorBlack)
   }
   
   func testHexFromThreeCharacters() {
