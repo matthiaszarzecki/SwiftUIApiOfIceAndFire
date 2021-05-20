27bf86310cb2aa28f6b474d4dedac295bc5603cc
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-05-20 19:00:33 +0200

    Add CharacterBasicHasInformationTests

---
+++ HousesOfWesteros/HousesOfWesterosTests/CharacterBasicTests/CharacterBasicHasInformationTests.swift
@@ -0,0 +1,33 @@
+//
+//  CharacterBasicHasInformationTests.swift
+//  HousesOfWesterosTests
+//
+//  Created by Matthias Zarzecki on 20.05.21.
+//
+
+import XCTest
+@testable import HousesOfWesteros
+
+class CharacterBasicHasInformationTests: XCTestCase {
+  func testHasInformation() {
+    // GIVEN a character that has some showable information
+    let character = MockClasses.character
+
+    // WHEN we call the "hasInformation" variable
+    let result = character.hasInformation
+
+    // THEN true is returned
+    XCTAssertTrue(result)
+  }
+
+  func testHasNoInformation() {
+    // GIVEN a character that has no showable information
+    let character = MockClasses.characterWithNoData
+
+    // WHEN we call the "hasInformation" variable
+    let result = character.hasInformation
+
+    // THEN false is returned
+    XCTAssertFalse(result)
+  }
+}
