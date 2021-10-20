1f70b5bc4deb0f7cb130a9003ce58dd064d93f24
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-10-20 17:24:27 +0200

    Add widget

---
+++ HousesOfWesteros/HousesOfWesterosWidget/HousesOfWesterosWidget.swift
@@ -0,0 +1,118 @@
+//
+//  HousesOfWesterosWidget.swift
+//  HousesOfWesterosWidget
+//
+//  Created by Matthias Zarzecki on 20.10.21.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+
+struct Provider: IntentTimelineProvider {
+  func placeholder(in context: Context) -> SimpleEntry {
+    SimpleEntry(
+      date: Date(),
+      text: "Placeholder",
+      configuration: ConfigurationIntent()
+    )
+  }
+
+  func getSnapshot(
+    for configuration: ConfigurationIntent,
+    in context: Context, completion: @escaping (SimpleEntry) -> ()
+  ) {
+    let entry = SimpleEntry(
+      date: Date(),
+      text: "Snapshot",
+      configuration: configuration)
+    completion(entry)
+  }
+
+  func getTimeline(
+    for configuration: ConfigurationIntent,
+    in context: Context, completion: @escaping (Timeline<Entry>) -> ()
+  ) {
+    var entries: [SimpleEntry] = []
+
+    var text = ""
+
+    Api.getSingleHouse(id: 43) { result in
+      switch result {
+      case .success(let receivedObject):
+        let house: HouseBasic = receivedObject
+        text = house.name
+
+        // Generate a timeline consisting of five entries
+        // an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+          let entryDate = Calendar.current.date(
+            byAdding: .minute,
+            value: hourOffset,
+            to: currentDate
+          )!
+          let entry = SimpleEntry(
+            date: entryDate,
+            text: "\(hourOffset) \(text)",
+            configuration: configuration
+          )
+          entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+
+      case .failure(let error):
+        print("Error! \(error)")
+      }
+    }
+  }
+}
+
+struct SimpleEntry: TimelineEntry {
+  let date: Date
+  let text: String
+  let configuration: ConfigurationIntent
+}
+
+struct HousesOfWesterosWidgetEntryView : View {
+  var entry: Provider.Entry
+
+  var body: some View {
+    VStack {
+      Text(entry.date, style: .time)
+      Text(entry.text)
+    }
+  }
+}
+
+@main
+struct HousesOfWesterosWidget: Widget {
+  let kind: String = "HousesOfWesterosWidget"
+
+  var body: some WidgetConfiguration {
+    IntentConfiguration(
+      kind: kind,
+      intent: ConfigurationIntent.self,
+      provider: Provider()
+    ) { entry in
+      HousesOfWesterosWidgetEntryView(entry: entry)
+    }
+    .configurationDisplayName("My Widget")
+    .description("This is an example widget.")
+  }
+}
+
+struct HousesOfWesterosWidget_Previews: PreviewProvider {
+  static var previews: some View {
+    HousesOfWesterosWidgetEntryView(
+      entry: SimpleEntry(
+        date: Date(),
+        text: "text",
+        configuration: ConfigurationIntent()
+      )
+    )
+    .previewContext(WidgetPreviewContext(family: .systemSmall))
+  }
+}
