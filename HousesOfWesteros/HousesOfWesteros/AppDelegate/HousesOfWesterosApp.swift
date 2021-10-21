0302d946f230f1bbabd2168af7434c9fd454ca99
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-10-21 18:24:02 +0200

    Add background ids

--- HousesOfWesteros/HousesOfWesteros/AppDelegate/HousesOfWesterosApp.swift
+++ HousesOfWesteros/HousesOfWesteros/AppDelegate/HousesOfWesterosApp.swift
@@ -5,13 +5,25 @@
 //  Created by Matthias Zarzecki on 13.04.21.
 //
 
+import BackgroundTasks
 import SwiftUI
 
 @main
 struct HousesOfWesterosApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+  
   var body: some Scene {
     WindowGroup {
       BaseView()
     }
+  }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+  ) -> Bool {
+    
   }
 }
