60657a9cccafbb983a6807f1182d8f9379bc1e81
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-10-25 16:57:10 +0200

    Remove faulty refresh code

--- HousesOfWesteros/HousesOfWesteros/AppDelegate/HousesOfWesterosApp.swift
+++ HousesOfWesteros/HousesOfWesteros/AppDelegate/HousesOfWesterosApp.swift
@@ -28,47 +28,9 @@ class AppDelegate: NSObject, UIApplicationDelegate {
       forTaskWithIdentifier: "com.matthiaszarzecki.HousesOfWesteros.backgroundRefresh",
       using: nil
     ) { task in
-         self.handleAppRefresh(task: task as! BGAppRefreshTask)
+         //self.handleAppRefresh(task: task as! BGAppRefreshTask)
     }
 
     return true
-  }
-
-  func scheduleAppRefresh() {
-     let request = BGAppRefreshTaskRequest(identifier: "com.matthiaszarzecki.HousesOfWesteros.backgroundRefresh")
-     // Fetch no earlier than 15 minutes from now.
-     request.earliestBeginDate = Date(timeIntervalSinceNow: 15 * 60)
-
-     do {
-        try BGTaskScheduler.shared.submit(request)
-     } catch {
-        print("Could not schedule app refresh: \(error)")
-     }
-  }
-
-  func handleAppRefresh(task: BGAppRefreshTask) {
-     // Schedule a new refresh task.
-     scheduleAppRefresh()
-
-     // Create an operation that performs the main part of the background task.
-     let operation = refreshAppContentsOperation()
-
-     // Provide the background task with an expiration handler that cancels the operation.
-     task.expirationHandler = {
-        operation.cancel()
-     }
-
-     // Inform the system that the background task is complete
-     // when the operation completes.
-     operation.completionBlock = {
-        task.setTaskCompleted(success: !operation.isCancelled)
-     }
-
-     // Start the operation.
-     operationQueue.addOperation(operation)
-   }
-
-  func refreshAppContentsOperation() {
-    print("### Hello ###")
   }
 }
