96d9ac00ea179491425166f3f547cf14442a8cb3
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-11-17 13:55:36 +0100

    Remove backgroundtasks

--- HousesOfWesteros/HousesOfWesteros/AppDelegate/HousesOfWesterosApp.swift
+++ HousesOfWesteros/HousesOfWesteros/AppDelegate/HousesOfWesterosApp.swift
@@ -5,32 +5,13 @@
 //  Created by Matthias Zarzecki on 13.04.21.
 //
 
-import BackgroundTasks
 import SwiftUI
 
 @main
 struct HousesOfWesterosApp: App {
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-  
   var body: some Scene {
     WindowGroup {
       BaseView()
     }
-  }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
-  ) -> Bool {
-    BGTaskScheduler.shared.register(
-      forTaskWithIdentifier: "com.matthiaszarzecki.HousesOfWesteros.backgroundRefresh",
-      using: nil
-    ) { task in
-         //self.handleAppRefresh(task: task as! BGAppRefreshTask)
-    }
-
-    return true
   }
 }
