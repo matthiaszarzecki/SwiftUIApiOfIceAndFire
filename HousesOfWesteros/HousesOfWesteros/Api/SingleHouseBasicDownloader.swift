afeda9e57b99520a84d5d4d9e714619c6a1c8d35
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2022-08-12 22:41:53 +0200

    Move SingleHouseBasicDownloaderProtocol to SingleHouseBasicDownloader

--- HousesOfWesteros/HousesOfWesteros/Api/SingleHouseBasicDownloader.swift
+++ HousesOfWesteros/HousesOfWesteros/Api/SingleHouseBasicDownloader.swift
@@ -5,6 +5,13 @@
 //  Created by Matthias Zarzecki on 09.07.21.
 //
 
+protocol SingleHouseBasicDownloaderProtocol {
+  func getSingleHouse(
+    id: Int,
+    completion: @escaping (Result<HouseBasic, NetworkError>) -> Void
+  )
+}
+
 class SingleHouseBasicDownloader: SingleHouseBasicDownloaderProtocol {
   func getSingleHouse(
     id: Int,
