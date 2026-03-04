d413688a4d7bda5a98d25d8c76c23d9d2017dc21
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2026-03-04 12:00:35 +0100

    Disable widget string identifiable extension part

--- HousesOfWesteros/HousesOfWesteros/Extensions/String-Extension-Main-App-Target-Only.swift
+++ HousesOfWesteros/HousesOfWesteros/Extensions/String-Extension-Main-App-Target-Only.swift
@@ -8,7 +8,7 @@
 // Apparently the widget-target already implements this protocol
 // for a string, and complains when it is re-implemented in the same
 // build-target. For now we create this one separately here.
-extension String: Identifiable {
+extension String {
   /// Necessary for the Identifiable-protocol.
   public var id: Self { self }
 }
